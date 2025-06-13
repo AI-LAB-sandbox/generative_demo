99c3f898c9c781065606e626a94d97605e030a33
lucple <dlwnstj99@naver.com>
2025-06-13 16:49:01 +0900

    🔥 [refactor] : all-in-one script for experiments

---
+++ script_shuttle.sh
@@ -0,0 +1,11 @@
+## 1. train vae 
+python train_vae.py --dataset Shuttle
+
+## 2. train classifier
+python train_classifier.py --dataset Shuttle
+
+## 3. visualize the results
+python vvs_shuttle.py
+
+## Check the directory ./visual !
+ 
